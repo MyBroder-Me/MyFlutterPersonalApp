c3c98a71a1d248b55fe55a22151af35aada59ece
Mykyta Ryasny <MykytaRyasny@gmail.com>
2025-02-05 18:48:52 +0100

    Fix: same library

--- lib/controller/pages/profile_page.dart
+++ lib/controller/pages/profile_page.dart
@@ -1,5 +1,4 @@
-// lib/auth/pages/profile_page.dart
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:myapp/controller/navigation.dart';
 import 'package:provider/provider.dart';
 
